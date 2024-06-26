067fd32c0db6a25a4161c3526e93b1db56c24ce6
ditrungduong <diduongtrung@gmail.com>
2024-06-26 10:04:48 -0700

    Update file

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 1111))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
