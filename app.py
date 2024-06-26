957803ac6c948f6dccd635fe4d423b58d26021bd
ditrungduong <94137484+ditrungduong@users.noreply.github.com>
2024-06-26 16:15:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import os
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/income.html')
+def income():
+    return render_template('income.html')
+
+@app.route('/expenses.html')
+def expenses():
+    return render_template('expenses.html')
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
