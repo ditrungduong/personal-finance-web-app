87801ac485b70e3836a68f43d3452f7a50b4bde1
ditrungduong <94137484+ditrungduong@users.noreply.github.com>
2024-06-27 20:16:31 -0700

    Switch to PyMySQL

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('CLEARDB_DATABASE_URL') or 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://b8e2fa0e4c74d9:d8f543d8@us-cluster-east-01.k8s.cleardb.net/heroku_6c38564e8dba3e8?reconnect=true'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-class Income(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    source = db.Column(db.String(80), nullable=False)
-    amount = db.Column(db.Float, nullable=False)
-    date = db.Column(db.Date, nullable=False)
 
 @app.route('/')
 def index():
@@ -21,37 +16,7 @@ def index():
 
 @app.route('/income')
 def income():
-    incomes = Income.query.all()
-    return render_template('income.html', incomes=incomes)
-
-@app.route('/income', methods=['POST'])
-def add_income():
-    data = request.get_json()
-    new_income = Income(source=data['source'], amount=data['amount'], date=data['date'])
-    db.session.add(new_income)
-    db.session.commit()
-    return jsonify({'message': 'Income added', 'income': {'source': new_income.source, 'amount': new_income.amount, 'date': new_income.date}})
-
-@app.route('/income/<int:id>', methods=['PUT'])
-def update_income(id):
-    data = request.get_json()
-    income = Income.query.get(id)
-    if income is None:
-        return jsonify({'message': 'Income not found'}), 404
-    income.source = data['source']
-    income.amount = data['amount']
-    income.date = data['date']
-    db.session.commit()
-    return jsonify({'message': 'Income updated'})
-
-@app.route('/income/<int:id>', methods=['DELETE'])
-def delete_income(id):
-    income = Income.query.get(id)
-    if income is None:
-        return jsonify({'message': 'Income not found'}), 404
-    db.session.delete(income)
-    db.session.commit()
-    return jsonify({'message': 'Income deleted'})
+    return render_template('income.html')
 
 @app.route('/expenses')
 def expenses():
